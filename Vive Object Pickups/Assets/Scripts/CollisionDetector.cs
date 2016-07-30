b91e17c594f79075510d3716406c84461b5cf2d1
gibyson6767 <scratchcoder700@yahoo.com>
2016-07-29 18:06:55 -0700

    Added Basic Collision Detection
    ...and cleaned up excess scripts

---
+++ Vive Object Pickups/Assets/Scripts/CollisionDetector.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionDetector : MonoBehaviour {
+	
+	void OnTriggerEnter(Collider colliderObj)
+	{
+		Debug.Log("I hit " + colliderObj.gameObject.name);
+	}
+
+}
